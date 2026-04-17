80c558a524981d7dd10e9cff66a0027790e20e2f
bujin21 <cvj0810@naver.com>
2026-04-17 17:53:17 +0900

    JWT보안 1일차

---
+++ menu/src/main/java/com/kh/menu/security/utils/CookieUtil.java
@@ -0,0 +1,28 @@
+package com.kh.menu.security.utils;
+
+import java.time.Duration;
+
+import org.springframework.http.ResponseCookie;
+
+public class CookieUtil {
+	public static final String ACCESS_COOKIE = "accessToken";
+	public static final String REFRESH_COOKIE = "refreshToken";
+	public static final String ROLE_COOKIE = "userRoles";
+	
+	public static ResponseCookie createTokenCookie(
+			String name, String value, long maxAgeDays
+			) {
+		return ResponseCookie.from(name, value)
+				.httpOnly(name.equals(REFRESH_COOKIE))
+				.secure(false) // 개발환경에서만 false
+				.path("/")
+				.sameSite("Lax")// CSRF방어
+				.maxAge(
+					maxAgeDays == 0 ? Duration.ZERO :
+					name.equals(REFRESH_COOKIE) ? 
+							Duration.ofDays(maxAgeDays) :
+							Duration.ofMinutes(maxAgeDays)
+						) //만료시간
+				.build();
+	}
+}
